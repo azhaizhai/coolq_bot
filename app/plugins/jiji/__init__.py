140bc971f8c233392c4a94809d89ac5f20114fed
Jay Yang <jay.y@shopperplus.com>
2020-03-13 12:25:12 +0800

    把四鸟踢了

--- app/plugins/jiji/__init__.py
+++ app/plugins/jiji/__init__.py
@@ -6,6 +6,5 @@ async def _(session: CommandSession):
 
 @on_natural_language(only_to_me=False)
 async def _(session: NLPSession):
-    # 只复读群消息，与没有对机器人说的话
-    if session.ctx['user_id'] == "1252584289":
-      return IntentCommand(60.0, 'remove_siniao', args={'message': session.msg})
+  if session.ctx['user_id'] == "1252584289":
+    return IntentCommand(60.0, 'remove_siniao')
