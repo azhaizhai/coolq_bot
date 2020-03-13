7c72b0b006b8c4d5f129a9d33120b99a8c9f2ddd
Jay Yang <jay.y@shopperplus.com>
2020-03-13 12:40:02 +0800

    把四鸟踢了

--- app/plugins/jiji/__init__.py
+++ app/plugins/jiji/__init__.py
@@ -4,4 +4,4 @@ from nonebot import on_natural_language, NLPSession
 @on_natural_language(only_to_me=False)
 async def _(session: NLPSession):
   if session.ctx['user_id'] == 1252584289:
-    session.send("把四鸟踢了!")
+    await session.send("把四鸟踢了!")
