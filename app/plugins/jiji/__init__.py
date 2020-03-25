abe0b70298684a2682ee2ac6dfef71e5a0cf987f
Jay Yang <1252584289@qq.com>
2020-03-25 14:16:54 +0800

    引入redis

--- app/plugins/jiji/__init__.py
+++ app/plugins/jiji/__init__.py
@@ -1,7 +1,13 @@
 from nonebot import on_natural_language, NLPSession
 import random
+import redis
 
 @on_natural_language(only_to_me=False)
 async def _(session: NLPSession):
+  conn = redis.Redis(host='127.0.0.1', port=6379)
   if session.ctx['user_id'] == 604853027 and random.random() < 0.05:
-    await session.send("把四鸟踢了!!")
+    if conn.set("604853027_warning", "0", ex = 600, nx=True):
+      await session.send("把四鸟踢了!")
+  
+  if session.ctx['user_id'] == 1252584289 and conn.set("1252584289_warning", "0", ex = 600, nx=True):
+    await session.send("把四鸟踢了!")
