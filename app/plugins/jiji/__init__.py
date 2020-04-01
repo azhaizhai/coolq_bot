558bf3bd255e9962bc49e12e5f7da956ec8b30a7
Jay Yang <1252584289@qq.com>
2020-04-01 12:52:24 +0800

    监控提到江叶的话

--- app/plugins/jiji/__init__.py
+++ app/plugins/jiji/__init__.py
@@ -10,10 +10,10 @@ async def _(session: NLPSession):
     if random.random() < 0.1 and redis.set("604853027_warning", "0", ex = 600, nx=True):
       await session.send("把四鸟踢了!")
   elif session.ctx['user_id'] == 1821703949:
-    if random.random() < 0.1 and redis.set("1821703949_warning", "0", ex = 600, nx=True):
+    if random.random() < 0.5 and redis.set("1821703949_warning", "0", ex = 600, nx=True):
       await session.send("毛玉哥哥我爱你＾3＾")
 
-@on_natural_language(keywords={'江叶', '浆液'})
+@on_natural_language(keywords={'江叶', '浆液', '桨叶'})
 async def _(session: NLPSession):
   if redis.set("1252584289_warning", "0", ex = 600, nx=True):
     await session.send("[CQ:at,qq=1252584289]  有人偷偷说你坏话")
